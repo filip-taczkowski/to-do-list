00d9446a882d917702fc6e5e7be1a1bc408176fc
Filip Taczkowski <filip.taczkowski@gmail.com>
2020-03-24 13:29:18 +0100

    Add remove method

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 
 class App extends React.Component {
+  state = {
+    tasks: [
+      {id: 1, name: 'Shopping'},
+      {id: 2, name: 'Go out with a dog'},
+
+    ],
+  }
+
+  componentDidMount() {
+    this.socket = io('http://localhost:8000');
+  }
+
+  removeTask(id) {
+    const { tasks } = this.state;
+    let index = '';
+
+    tasks.filter(task => {
+      if (task.id === id) {
+        index = tasks.indexOf(task);
+      }
+      return index;
+    });
+
+    this.setState({
+      tasks: tasks.filter(task => task.id !== id),
+    });
+
+    this.socket.emit('removeTask', index);
+  }
 
   render() {
+    const { tasks } = this.state;
+
     return (
       <div className="App">
 
@@ -15,8 +47,12 @@ class App extends React.Component {
           <h2>Tasks</h2>
 
           <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+            {tasks.map(task => (
+              <li key={task.id} className='task'>
+                {task.name}
+                <button className="btn btn--red" onClick={() => this.removeTask(task.id)}>Remove</button>
+              </li>
+            ))}
           </ul>
 
           <form id="add-task-form">

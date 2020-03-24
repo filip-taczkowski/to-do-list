0fa44f4fd43b3036188166f2d7daf24249a03c8a
Filip Taczkowski <filip.taczkowski@gmail.com>
2020-03-24 13:01:33 +0100

    Add socket.it and update server

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+
+const tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    console.log(`New task found: ${task}`);
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  
+  socket.on('removeTask', (index) => {
+    console.log(`Task ${index} has been removed`);
+    tasks.splice(index, 1);
+    socket.broadcast.emit('removeTask');
+  });
+})
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
